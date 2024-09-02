ec1689d2401654fbaa8cbf92e9d01b674e22eff0
a7medsaber10 <ahmedsa2522003@gmail.com>
2024-09-02 03:48:52 +0300

    Add project files.

---
+++ AppStructure/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppStructure.Controllers
+{
+    public class MovieController : Controller
+    {
+        // Action --> Public non static function => Movie/GetMovie
+        public IActionResult GetMovie(int id)
+        {
+            //ContentResult result = new ContentResult();
+            //result.Content = $"Movie with id = {id}";
+
+            return Content($"Movie with id = {id}");
+        }
+
+        [ActionName( "index" )]
+        [HttpPost]
+        [AcceptVerbs("Post", "Get")]
+        public IActionResult Index()
+        {
+            return RedirectToAction(nameof(GetMovie), new {id = 10});
+        }
+    }
+}
